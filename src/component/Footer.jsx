1a554253ab04542c46b1fce5a6048cb02fcb6d4c
MohammadMusebAkbani <akbanimuseb@gmail.com>
2025-09-29 11:01:26 +0530

    Added Icons to footer component

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Button from "./Button";
+import { BsFacebook } from "react-icons/bs";
+import { RiInstagramFill } from "react-icons/ri";
+import { SiWhatsapp } from "react-icons/si";
 
 function Footer() {
   return (
     <div className="py-5 px-3 bg-amber-200">
-      <header
-        className="px-[1rem] m-auto text-xl pb-8 grid grid-cols-5 max-[400px]:grid-cols-1 max-[770px]:gap-[2rem] max-[770px]:grid-cols-2"
-      >
+      <header className="px-[1rem] m-auto text-xl pb-8 grid grid-cols-5 max-[400px]:grid-cols-1 max-[770px]:gap-[2rem] max-[770px]:grid-cols-2">
         <h1 className="text-3xl">
           <span className="text-[tomato] font-extrabold">B</span>rand
         </h1>
@@ -61,26 +61,33 @@ function Footer() {
           ad.
         </p>
         <div className="flex gap-8">
-          <div className="bg-amber-400 w-7 h-7 rounded-full flex items-center justify-center 
-          border-black border-1">
-            <a href="" className="font-bold">
-              X
+          <div className=" w-7 h-7 rounded-full">
+            <a
+              href=""
+              className="w-full h-full flex items-center justify-center"
+            >
+              <BsFacebook size="28" className="w-full h-full text-blue-950" />
             </a>
           </div>
-          <div className="bg-amber-400 w-7 h-7 rounded-full flex items-center justify-center border-black border-1">
-            <a href="" className="font-bold">
-              Y
+          <div className=" w-7 h-7 rounded-full text-red-950">
+            <a
+              href=""
+              className="w-full h-full flex items-center justify-center"
+            >
+              <RiInstagramFill size="28" className="w-full h-full" />
             </a>
           </div>
-           <div className="bg-amber-400 w-7 h-7 rounded-full flex items-center justify-center  border-black border-1">
-            <a href="" className="font-bold">
-              Z
+          <div className=" w-7 h-7 rounded-full">
+            <a
+              href=""
+              className="w-full h-full flex items-center justify-center"
+            >
+              <SiWhatsapp size="28" className="w-full h-full" />
             </a>
           </div>
         </div>
       </div>
     </div>
-    
   );
 }
 
